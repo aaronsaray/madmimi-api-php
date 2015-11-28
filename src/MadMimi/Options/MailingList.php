a7a173ddf63dfdd4f118aa8c77db75eb2bcb2b75
Aaron Saray <me@aaronsaray.com>
2015-11-28 11:42:51 -0600

    Refactored to use a trait to make the code less dupey.

--- src/MadMimi/Options/MailingList.php
+++ src/MadMimi/Options/MailingList.php
@@ -6,7 +6,6 @@
  */
 
 namespace MadMimi\Options;
-use Symfony\Component\Yaml\Yaml;
 
 /**
  * Class MailingList
@@ -16,45 +15,7 @@ use Symfony\Component\Yaml\Yaml;
  */
 class MailingList extends OptionsAbstract
 {
-    /**
-     * @var string the name of the promotion in MadMimi
-     */
-    protected $promotion_name;
-
-    /**
-     * @var string Subject of the email
-     */
-    protected $subject;
-
-    /**
-     * @var string the from email in format of "name <email>"
-     */
-    protected $from;
-
-    /**
-     * @var string raw HTML to send with this transactional email
-     */
-    protected $raw_html;
-
-    /**
-     * @var string raw plain text to send with this email
-     */
-    protected $raw_plain_text;
-
-    /**
-     * @var string the YAML for replacing placeholders
-     */
-    protected $body;
-
-    /**
-     * @var string a boolean representation
-     */
-    protected $track_links;
-
-    /**
-     * @var string a boolean representation
-     */
-    protected $hidden;
+    use PromotionBaseTrait;
 
     /**
      * @var string a csv list of mailing lists to add to
@@ -85,95 +46,6 @@ class MailingList extends OptionsAbstract
     public function getRequestType()
     {
         return self::REQUEST_TYPE_POST;
-    }
-
-    /**
-     * @param string $promotionName
-     * @return $this
-     */
-    public function setPromotionName($promotionName)
-    {
-        $this->promotion_name = $promotionName;
-        return $this;
-    }
-
-    /**
-     * @param string $subject
-     * @return $this
-     */
-    public function setSubject($subject)
-    {
-        $this->subject = $subject;
-        return $this;
-    }
-
-    /**
-     * Sets the from address with optional name
-     * @param $email
-     * @param string $name
-     * @return $this
-     */
-    public function setFrom($email, $name = '')
-    {
-        return $this->setEmailAddress('from', $email, $name);
-    }
-
-    /**
-     * Use this to set placeholders - generates a YAML and will overwrite your old values
-     *
-     * @param array $placeholders by key => value of placeholder => replacement
-     * @return $this
-     */
-    public function setPlaceholderValues(array $placeholders)
-    {
-        $this->body = Yaml::dump($placeholders);
-        return $this;
-    }
-
-    /**
-     * Set plain HTML instead of using a prebuilt promotion value
-     *
-     * @param $html string the raw html to send
-     * @return $this
-     */
-    public function setHTML($html)
-    {
-        $this->raw_html = $html;
-        return $this;
-    }
-
-    /**
-     * Set plain text content of promotion
-     *
-     * @param $text string the raw plain text to send
-     * @return $this
-     */
-    public function setText($text)
-    {
-        $this->raw_plain_text = $text;
-        return $this;
-    }
-
-    /**
-     * Track links
-     *
-     * @param bool|true $bool
-     * @return $this
-     */
-    public function setTrackLinks($bool = true)
-    {
-        return $this->setTranslatedBooleanValue('track_links', $bool);
-    }
-
-    /**
-     * Hides the promotion from the web interface
-     *
-     * @param bool|true $bool
-     * @return $this
-     */
-    public function setHidden($bool = true)
-    {
-        return $this->setTranslatedBooleanValue('hidden', $bool);
     }
 
     /**
--- src/MadMimi/Options/Transactional.php
+++ src/MadMimi/Options/Transactional.php
@@ -6,7 +6,6 @@
  */
 
 namespace MadMimi\Options;
-use Symfony\Component\Yaml\Yaml;
 
 /**
  * Class Transactional
@@ -16,25 +15,12 @@ use Symfony\Component\Yaml\Yaml;
  */
 class Transactional extends OptionsAbstract
 {
-    /**
-     * @var string the name of the promotion in MadMimi
-     */
-    protected $promotion_name;
-
-    /**
-     * @var string Subject of the email
-     */
-    protected $subject;
+    use PromotionBaseTrait;
 
     /**
      * @var string the Recipient of the email in format of "name <email>"
      */
     protected $recipients;
-
-    /**
-     * @var string the from email in format of "name <email>"
-     */
-    protected $from;
 
     /**
      * @var string cc email address
@@ -52,34 +38,9 @@ class Transactional extends OptionsAbstract
     protected $sender;
 
     /**
-     * @var string raw HTML to send with this transactional email
-     */
-    protected $raw_html;
-
-    /**
-     * @var string raw plain text to send with this email
-     */
-    protected $raw_plain_text;
-
-    /**
-     * @var string the YAML for replacing placeholders
-     */
-    protected $body;
-
-    /**
      * @var string a boolean representation
      */
     protected $check_suppressed;
-
-    /**
-     * @var string a boolean representation
-     */
-    protected $track_links;
-
-    /**
-     * @var string a boolean representation
-     */
-    protected $hidden;
 
     /**
      * @var string a boolean representation
@@ -108,26 +69,6 @@ class Transactional extends OptionsAbstract
     }
 
     /**
-     * @param string $promotionName
-     * @return $this
-     */
-    public function setPromotionName($promotionName)
-    {
-        $this->promotion_name = $promotionName;
-        return $this;
-    }
-
-    /**
-     * @param string $subject
-     * @return $this
-     */
-    public function setSubject($subject)
-    {
-        $this->subject = $subject;
-        return $this;
-    }
-
-    /**
      * Sets the to address with optional name
      * @param $email
      * @param string $name
@@ -136,17 +77,6 @@ class Transactional extends OptionsAbstract
     public function setTo($email, $name = '')
     {
         return $this->setEmailAddress('recipients', $email, $name);
-    }
-
-    /**
-     * Sets the from address with optional name
-     * @param $email
-     * @param string $name
-     * @return $this
-     */
-    public function setFrom($email, $name = '')
-    {
-        return $this->setEmailAddress('from', $email, $name);
     }
 
     /**
@@ -183,42 +113,6 @@ class Transactional extends OptionsAbstract
     }
 
     /**
-     * Use this to set placeholders - generates a YAML and will overwrite your old values
-     *
-     * @param array $placeholders by key => value of placeholder => replacement
-     * @return $this
-     */
-    public function setPlaceholderValues(array $placeholders)
-    {
-        $this->body = Yaml::dump($placeholders);
-        return $this;
-    }
-
-    /**
-     * Set plain HTML instead of using a prebuilt promotion value
-     *
-     * @param $html string the raw html to send
-     * @return $this
-     */
-    public function setHTML($html)
-    {
-        $this->raw_html = $html;
-        return $this;
-    }
-
-    /**
-     * Set plain text content of promotion
-     *
-     * @param $text string the raw plain text to send
-     * @return $this
-     */
-    public function setText($text)
-    {
-        $this->raw_plain_text = $text;
-        return $this;
-    }
-
-    /**
      * Check suppressed
      *
      * @param bool|true $bool
@@ -227,28 +121,6 @@ class Transactional extends OptionsAbstract
     public function setCheckSuppressed($bool = true)
     {
         return $this->setTranslatedBooleanValue('check_suppressed', $bool);
-    }
-
-    /**
-     * Track links
-     *
-     * @param bool|true $bool
-     * @return $this
-     */
-    public function setTrackLinks($bool = true)
-    {
-        return $this->setTranslatedBooleanValue('track_links', $bool);
-    }
-
-    /**
-     * Hides the promotion from the web interface
-     *
-     * @param bool|true $bool
-     * @return $this
-     */
-    public function setHidden($bool = true)
-    {
-        return $this->setTranslatedBooleanValue('hidden', $bool);
     }
 
     /**
